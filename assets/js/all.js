9f757207de432b0cd02cc8a68a5e1c405b398089
amy <amy>
2022-08-26 16:40:42 +0800

    Update 2022-08-26T08:40:09.646Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,54 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  // 嚴選療程
+  var swiper = new Swiper(".swiper-course", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    //多欄
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 8
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 8
+      }
+    },
+    loop: true,
+    // speed: 1500,
+    // autoplay: {
+    //   disableOnInteraction: false,
+    //   delay: 0
+    // },
+    // 左右箭頭
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  }); // 環境介紹
+
+  var swiperSpace = new Swiper(".swiper-space", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    //多欄
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 8
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 8
+      }
+    },
+    loop: true,
+    // 左右箭頭
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
