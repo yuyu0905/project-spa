ebb08c61c1def2c18b84919e9f2cd08e30b7be4f
amy <amy>
2022-08-26 16:38:28 +0800

    首頁設計稿部分 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,53 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  // 嚴選療程
+  const swiper = new Swiper(".swiper-course", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    //多欄
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 8,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 8,
+      }
+    },
+    loop: true,
+    // speed: 1500,
+    // autoplay: {
+    //   disableOnInteraction: false,
+    //   delay: 0
+    // },
+    // 左右箭頭
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  // 環境介紹
+  const  swiperSpace = new Swiper(".swiper-space", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    //多欄
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 8,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 8,
+      }
+    },
+    loop: true,
+    // 左右箭頭
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
 });
