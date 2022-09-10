ec9e153abff7a09b556c4f41e5a20799b526652b
amy <amy>
2022-09-11 01:00:49 +0800

    預約頁面 完成

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -66,7 +66,36 @@ $(document).ready(function () {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  // 日期套件
+  const elem = document.getElementById('reserve-date');
+  if(elem) {
+    const datepicker = new Datepicker(elem, {
+      format: "yyyy/mm/dd",
+      language: "zh-TW",
+      todayBtn: true,
+      todayBtnMode: 1,
+      todayHighlight: true,
+    });
   
+    elem.addEventListener('changeDate', function(e) {
+      console.log('choose date is', datepicker.getDate("yyyy/mm/dd"));
+    });
+  }
+
+  // 選擇按摩師
+  $(".service-reserve-masseur").click(function(e) {
+    $(this).addClass('active');
+    $(this).parent().siblings().children('div').removeClass('active');
+  });
+
+  // 預約時間
+  $(".service-reserve-time-btn").click(function(e) {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  });
+
+
 });
 
 function login() {
@@ -77,18 +106,38 @@ function login() {
   }
 
   let loginModal = document.getElementById('loginModal');
-  var modal = bootstrap.Modal.getInstance(loginModal);
+  let modal = bootstrap.Modal.getInstance(loginModal);
   modal.hide();
   localStorage.setItem("login", "login");
   $("#login-btn").addClass("d-none");
   $("#user-btn").removeClass("d-none");
+
+  if(!!localStorage.getItem("reserve")) {
+    window.location.href = "/service-reserve.html";
+    localStorage.removeItem("reserve");
+  }
 }
 
 function logout() {
   let logoutModal = document.getElementById('logoutModal');
-  var modal = bootstrap.Modal.getInstance(logoutModal);
+  let modal = bootstrap.Modal.getInstance(logoutModal);
   modal.hide();
   localStorage.removeItem("login");
   $("#login-btn").removeClass("d-none");
   $("#user-btn").addClass("d-none");
+}
+
+function goReserve() {
+  // 確認是否登入
+  let isLogin = !!localStorage.getItem("login");
+  if(isLogin) {
+    window.location.href = "/service-reserve.html";
+  } else {
+    localStorage.setItem("reserve", "reserve");
+    let loginModal = document.getElementById('loginModal');
+    let modal = new bootstrap.Modal(document.getElementById('loginModal'), {
+      keyboard: false
+    })
+    modal.show(loginModal);
+  }
 }
